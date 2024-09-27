0dcd7dd8715f846340e06d08101b677f979d74c6
Dean <radean.rashed@gmail.com>
2024-09-27 14:45:19 -0400

    Update MeepMeep and add leftRed path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,64 +1,69 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.core.util.FieldUtil;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.util.FieldUtil;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static final double TILE_SIZE = 24;
 
-    private static void strafe(TrajectorySequenceBuilder builder, double x, double y) {
-        builder.strafeTo(new Vector2d(x * TILE_SIZE, y * TILE_SIZE));
-    }
-
-    private static void line(TrajectorySequenceBuilder builder, double x, double y, double heading) {
-        builder.lineToLinearHeading(new Pose2d(x * TILE_SIZE, y * TILE_SIZE, Math.toRadians(heading)));
-    }
-
-    private static void wait(TrajectorySequenceBuilder builder, double seconds) {
-        builder.waitSeconds(seconds);
-    }
-
-    private static void spline(TrajectorySequenceBuilder builder, double x, double y, double heading) {
-        builder.splineTo(new Vector2d(x * TILE_SIZE, y * TILE_SIZE), Math.toRadians(heading));
-    }
-
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(720);
+        MeepMeep meepMeep = new MeepMeep(800);
         FieldUtil.setFIELD_HEIGHT(144);
         FieldUtil.setFIELD_WIDTH(144);
 
+        int mode = 1;
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> {
-                    var builder = drive.trajectorySequenceBuilder(new Pose2d(12, -72, Math.toRadians(90)));
-                    strafe(builder, 0.25, -1.25);
-                    wait(builder, 1); // deposit first specimen
-                    strafe(builder, 2, -1.5);
-                    wait(builder, 1); // pick up first sample
-                    line(builder, -1.5, -1.5, 180);
-                    spline(builder, -2.5, -2.25, 270);
-                    wait(builder, 1); // deposit first sample
-                    line(builder, -2.5, -1.5, 180);
-                    line(builder, 2.5, -1.5, 90);
-                    wait(builder, 1); // pick up second sample
-                    line(builder, -1.5, -1.5, 180);
-                    spline(builder, -2.5, -2.25, 270);
-                    wait(builder, 1); // deposit second sample
-                    strafe(builder, -2.5, -1.5);
-                    strafe(builder, 2.5, -1.5);
-                    strafe(builder, 2.5, -2.5); // park
+                .build();
 
-                    return builder.build();
-                });
+        switch (mode) {
+            case 0:
+                myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, -72, Math.toRadians(90)))
+                        .strafeTo(new Vector2d(0.25 * TILE_SIZE, -1.25 * TILE_SIZE))
+                        .waitSeconds(1) // deposit first specimen
+                        .strafeTo(new Vector2d(2 * TILE_SIZE, -1.5 * TILE_SIZE))
+                        .waitSeconds(1) // pick up first sample
+                        .strafeToLinearHeading(new Vector2d(-1.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(180))
+                        .strafeToLinearHeading(new Vector2d(-2.5 * TILE_SIZE, -2.25 * TILE_SIZE), Math.toRadians(270))
+                        .waitSeconds(1) // deposit first sample
+                        .strafeToLinearHeading(new Vector2d(-2.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(180))
+                        .strafeToLinearHeading(new Vector2d(2.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(90))
+                        .waitSeconds(1) // pick up second sample
+                        .strafeToLinearHeading(new Vector2d(-1.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(180))
+                        .splineTo(new Vector2d(-2.5 * TILE_SIZE, -2.25 * TILE_SIZE), Math.toRadians(270))
+                        .waitSeconds(1) // deposit second sample
+                        .strafeTo(new Vector2d(-2.5 * TILE_SIZE, -1.5 * TILE_SIZE))
+                        .strafeTo(new Vector2d(2.5 * TILE_SIZE, -1.5 * TILE_SIZE))
+                        .strafeTo(new Vector2d(2.5 * TILE_SIZE, -2.5 * TILE_SIZE)) // park
+                        .build());
 
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+            case 1:
+                myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-12, -72, Math.toRadians(90)))
+                                .strafeTo(new Vector2d(-0.25 * TILE_SIZE, -1.25 * TILE_SIZE))
+                                .waitSeconds(1) // deposit first specimen
+                                .strafeToLinearHeading(new Vector2d(-1.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(135))
+                                .waitSeconds(1) // pick up first sample
+                                .strafeToLinearHeading(new Vector2d(-2 * TILE_SIZE, -2.5 * TILE_SIZE), Math.toRadians(200))
+                                .waitSeconds(1) // deposit first sample
+                                .strafeToLinearHeading(new Vector2d(-2 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(135))
+                                .waitSeconds(1) // pick up second sample
+                                .strafeToLinearHeading(new Vector2d(-2 * TILE_SIZE, -2.5 * TILE_SIZE), Math.toRadians(200))
+                                .waitSeconds(1) // deposit second sample
+                                .strafeToLinearHeading(new Vector2d(-2.3 * TILE_SIZE, -1.6 * TILE_SIZE), Math.toRadians(135))
+                                .waitSeconds(1) // pick up third sample
+                                .strafeToLinearHeading(new Vector2d(-2 * TILE_SIZE, -2.5 * TILE_SIZE), Math.toRadians(200))
+                                .waitSeconds(1) // deposit third sample
+                                .strafeToLinearHeading(new Vector2d(-1.5 * TILE_SIZE, -1.5 * TILE_SIZE), Math.toRadians(0))
+                                .strafeTo(new Vector2d(2.5 * TILE_SIZE, -1.5 * TILE_SIZE))
+                                .strafeTo(new Vector2d(2.5 * TILE_SIZE, -2.5 * TILE_SIZE)) // park
+                                .build());
+        }
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
