fc2cda9bd2688463636996448cee7b758394b838
OoEthanoO <ethanxucoder@gmail.com>
2024-10-10 08:15:29 -0400

    Complete claw align prototype

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClawAlignOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClawAlignOpMode.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.EOCV.vision.RedCenterDetectionPipeline;
 public class ClawAlignOpMode extends OpMode {
     private CRServo crServo;
     private final double TARGET_ANGLE = 0;
+    private final double ANGLE_THRESHOLD = 5;
     private boolean buttonPressed = false;
     private RedCenterDetectionPipeline pipeline;
 
@@ -18,11 +19,33 @@ public class ClawAlignOpMode extends OpMode {
         crServo = hardwareMap.get(CRServo.class, "servo");
     }
 
+    public double clockwiseDistance(double currentAngle, double targetAngle) {
+        return (currentAngle - targetAngle + 360) % 360;
+    }
+
+    public double counterClockwiseDistance(double currentAngle, double targetAngle) {
+        return (targetAngle - currentAngle + 360) % 360;
+    }
+
+    public double minDistance(double currentAngle, double targetAngle) {
+        return Math.min(clockwiseDistance(currentAngle, targetAngle), counterClockwiseDistance(currentAngle, targetAngle));
+    }
+
+    public double flipAngle(double angle) {
+        return (angle + 360) % 360 - 180;
+    }
+
     @Override
     public void loop() {
         if (buttonPressed) {
             double angle = pipeline.getAngle();
-            boolean clockwise =
+            angle = (minDistance(flipAngle(angle), TARGET_ANGLE) < minDistance(angle, TARGET_ANGLE)) ? flipAngle(angle) : angle;
+
+            if (angle > ANGLE_THRESHOLD) {
+                crServo.setPower(clockwiseDistance(angle, TARGET_ANGLE) < counterClockwiseDistance(angle, TARGET_ANGLE) ? 1 : -1);
+            } else {
+                crServo.setPower(0);
+            }
         }
     }
 }
