7162c4057babcf1c4a0d19355992da977a8699ff
OoEthanoO <ethanxucoder@gmail.com>
2024-10-15 15:58:03 -0400

    Change button press to press from hold

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClawAlignOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClawAlignOpMode.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -11,25 +12,26 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
-@TeleOp(name="Claw Align OpMode", group = "TeleOp")
+@TeleOp(name = "Claw Align OpMode", group = "TeleOp")
 public class ClawAlignOpMode extends OpMode {
     private CRServo crServo;
     private RedCenterDetectionPipeline pipeline;
     private OpenCvWebcam camera;
-    private boolean buttonPressed = false;
+    private boolean running = false;
 
     @Override
     public void init() {
-        crServo = hardwareMap.get(CRServo.class, "servo");
+        crServo = hardwareMap.get(CRServo.class, "wobbleGrabber");
         pipeline = new RedCenterDetectionPipeline();
 
         int cameraMonitoryViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitoryViewId);
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "camera"), cameraMonitoryViewId);
         camera.setPipeline(pipeline);
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
                 camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                FtcDashboard.getInstance().startCameraStream(camera, 30);
             }
 
             @Override
@@ -51,28 +53,21 @@ public class ClawAlignOpMode extends OpMode {
         return Math.min(clockwiseDistance(currentAngle, targetAngle), counterClockwiseDistance(currentAngle, targetAngle));
     }
 
-    public double flipAngle(double angle) {
-        return (angle + 360) % 360 - 180;
-    }
-
     @Override
     public void loop() {
-        if (gamepad1.a) {
-            buttonPressed = true;
+        if (gamepad1.a && !running) {
+            running = true;
+        }
+
+        if (running) {
             double angle = pipeline.getAngle();
             double TARGET_ANGLE = 0;
-            angle = (minDistance(flipAngle(angle), TARGET_ANGLE) < minDistance(angle, TARGET_ANGLE)) ? flipAngle(angle) : angle;
-
             double ANGLE_THRESHOLD = 5;
             if (minDistance(angle, TARGET_ANGLE) > ANGLE_THRESHOLD) {
                 crServo.setPower(clockwiseDistance(angle, TARGET_ANGLE) < counterClockwiseDistance(angle, TARGET_ANGLE) ? 1 : -1);
             } else {
                 crServo.setPower(0);
-            }
-        } else {
-            if (buttonPressed) {
-                crServo.setPower(0);
-                buttonPressed = false;
+                running = false;
             }
         }
     }
