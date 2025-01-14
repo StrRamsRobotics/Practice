e55509c407f4b19fc9209d778f057225c1b176bb
kiamehrjj <kiamehrjj.mail@gmail.com>
2025-01-14 15:37:49 -0500

    Deleted claw related commands in teleop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Teleop.java
@@ -29,64 +29,9 @@ public class Teleop extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double y = -gamepad1.left_stick_y;
             double rx = gamepad1.right_stick_x;
-
             chassis.drive.setDrivePowers(new PoseVelocity2d(new Vector2d(x, y), rx));
-
-            // Claw open/close
-
-            if (gamepad2.left_trigger >= 0.5 && gamepad2.left_trigger <= 1) {
-                chassis.claw.setPower(1);
-            }
-            if (gamepad2.right_trigger >= 0.5 && gamepad2.right_trigger <= 1) {
-                chassis.claw.setPower(-1);
-            }
-
-            // Claw pickup
-
-            if (gamepad1.y && !isYHeld) {
-                isYHeld = true;
-                pickup();
-            }
-
-            if (!gamepad1.y && isYHeld) {
-                isYHeld = false;
-                double armAngle = chassis.armPivot1.getCurrentPosition() * 180; // angle of the arm
-                chassis.clawPitch.setPosition((90 - armAngle) / 180); // make the claw perpendicular to the ground
-            }
-
-            // Claw alignment
-
-            if (gamepad1.a && !isAHeld) {
-                isAHeld = true;
-                alignClaw();
-            }
-
-            if (!gamepad1.a) {
-                isAHeld = false;
-            }
-        }
     }
 
-    private void pickup() {
-        chassis.armPivot1.setTargetPosition(90);
-        chassis.armPivot2.setTargetPosition(90);
-        chassis.slidesPivot1.setTargetPosition(90);
-        chassis.slidesPivot2.setTargetPosition(90);
-    }
-
-    private void alignClaw() {
-        double angle;
-        if (Chassis.isBlue) {
-            angle = chassis.bluePipeline.angle;
-        } else {
-            angle = chassis.redPipeline.angle;
-        }
-        double TARGET_ANGLE = 0;
-        double ANGLE_THRESHOLD = 5;
-        if (minDistance(angle, TARGET_ANGLE) > ANGLE_THRESHOLD) {
-            chassis.clawRotate.setPosition(angle / 180);
-        }
-    }
 
     public double clockwiseDistance(double currentAngle, double targetAngle) {
         return (currentAngle - targetAngle + 360) % 360;
